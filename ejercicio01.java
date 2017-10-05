aa546c0356d33821cc987e5f994d58e5827d86e5
Beatriz Parejo Ramos <artismusas@gmail.com>
2017-10-05 10:22:30 +0200

    Añadido el programa ejercicio01

---
+++ ejercicio01.java
@@ -0,0 +1,12 @@
+/**
+ * Ejercicios 1,2
+ *
+ */
+
+public class ejercicio01 { // Clase principal
+  public static void main(String[] args) {
+    System.out.println("Nombre:Beatriz");
+    System.out.println ("Dirección: Málaga, Calle Marie Curie, nº10");
+    System.out.println ("Teléfono: 6443343424546");
+  }
+}
