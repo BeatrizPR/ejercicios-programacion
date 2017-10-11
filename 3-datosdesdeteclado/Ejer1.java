eb774240667607ff0478518247cc1e217926e1ca
Beatriz Parejo Ramos <artismusas@gmail.com>
2017-10-12 00:00:22 +0200

    Añadiendo los ejercicios del tema 3

--- 3-datosdesdeteclado/Ejer1.java
+++ 3-datosdesdeteclado/Ejer1.java
@@ -8,18 +8,14 @@ public class Ejer1 {
       * int primerNumero = Integer.parseInt(System.console().readLine()); */
 
     System.out.print("Por favor, introduce un número con o sin decimales: ");
-    linea = System.console().readLine();
-    double primerNumero = Double.parseDouble( linea );
+    double primerNumero = Double.parseDouble( System.console().readLine());
 
     System.out.print("introduce otro, por favor: ");
-    linea = System.console().readLine();
-    double segundoNumero = Double.parseDouble( linea );
+    double segundoNumero = Double.parseDouble( System.console().readLine() );
 
-    double total = (2 * primerNumero) + segundoNumero;
+    double total = primerNumero * segundoNumero;
 
-    System.out.print("El primer número introducido es " + primerNumero);
-    System.out.println(" y el segundo es " + segundoNumero);
-    System.out.print("El doble del primer número más el segundo es " + total);
+    System.out.print("La multiplicación del primer número y el segundo número es " + total);
     
   } 
 }
